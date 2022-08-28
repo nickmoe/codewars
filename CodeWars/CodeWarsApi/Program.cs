4ee6c6d3e875eccab7a461018e4a871eba1e6a60
Nick M <nmoeller96@hotmail.com>
2022-08-28 17:53:47 -0500

    save

--- CodeWars/CodeWarsApi/Program.cs
+++ CodeWars/CodeWarsApi/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace CodeWarsGenerator
+namespace Kata
 {
     internal class Program
     {
--- CodeWars/Kata/Program.cs
+++ CodeWars/Kata/Program.cs
@@ -6,7 +6,7 @@ namespace Kata
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("kata!");
         }
     }
 }
